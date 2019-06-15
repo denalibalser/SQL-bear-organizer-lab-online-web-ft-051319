3efc684c749b9f71251b74e8a6649f5f2e4292a3
Denali Balser <denali.balser167@gmail.com>
2019-06-15 12:23:16 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,15 +1,15 @@
-INSERT INTO bears(name, age, gender, color, temperment, alive) VALUES("Mr. Chocolate", 20, "M", "dark brown", "calm", 0);
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ("Mr. Chocolate", 20, "M", "dark brown", "calm", 0);
 
-INSERT INTO bears(name, age, gender, color, temperment, alive) VALUES("Rowdy", 10, "M", "black", "intense", 1);
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES  ("Rowdy", 10, "M", "black", "intense", 1);
 
-INSERT INTO bears(name, age, gender, color, temperment, alive) VALUES("Tabitha", 6, "F", "dark brown", "Nice", 1);
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ("Tabitha", 6, "F", "dark brown", "Nice", 1);
 
-INSERT INTO bears(name, age, gender, color, temperment, alive) VALUES("Sargeant Brown", 19, "M", "Green", "Slimy", 0);
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ("Sargeant Brown", 19, "M", "Green", "Slimy", 0);
 
-INSERT INTO bears(name, age, gender, color, temperment, alive) VALUES("Melissa", 13, "F", "dark brown", "goofy", 1);
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES  ("Melissa", 13, "F", "dark brown", "goofy", 1);
 
-INSERT INTO bears(name, age, gender, color, temperment, alive) VALUES("Grinch", 2, "M", "Black", "Grinchy", 1);
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ("Grinch", 2, "M", "Black", "Grinchy", 1);
 
-INSERT INTO bears(name, age, gender, color, temperment, alive) VALUES('Wendy', 7, 'F', 'brown', 'playful', 1);
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ('Wendy', 7, 'F', 'brown', 'playful', 1);
 
-INSERT INTO bears(name, age, gender, color, temperment, alive) VALUES(null, 20, "M", "black", "aggressive", 0);
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES (NULL, 20, "M", "black", "aggressive", 0);
